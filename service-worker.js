da124b8bcdece7071760d2fb146c71598c711e7c
amartinsbraganca <a.martins.braganca@gmail.com>
2025-09-04 14:07:58 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = "quiz-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
+// Instala e guarda em cache
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Usa cache primeiro, depois rede
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
